f67705954ca2df7584fe172eac70362a831c6136
Nicolas Veloz Savino <nicolas.veloz.savino@gmail.com>
2016-09-19 04:55:47 -0500

    fixing config file

--- src/emqttd_mios_plugin_app.erl
+++ src/emqttd_mios_plugin_app.erl
@@ -19,7 +19,7 @@
 start(_StartType, _StartArgs) ->
   gen_conf:init(?APP),
   {ok, Sup} = emqttd_mios_plugin_sup:start_link(),
-  {ok, Opts} = gen_conf:value(?APP,mios),
+  Opts = gen_conf:value(?APP,mios),
   emqttd_mios_plugin:load([]),
   ok = emqttd_access_control:register_mod(auth, emqttd_mios_plugin_auth, Opts),
   ok = emqttd_access_control:register_mod(acl, emqttd_mios_plugin_acl, []),
