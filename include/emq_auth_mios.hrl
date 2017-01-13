8d6048a22a4b2bde01e2aa9e42231f529f50ecc3
Nicolas Veloz Savino <nveloz@mios.com>
2017-01-13 15:21:54 -0500

    Fix logger

--- include/emq_auth_mios.hrl
+++ include/emq_auth_mios.hrl
@@ -22,4 +22,4 @@
 -define(LOG_LV(Level,Format,Args),
   io:format("~p~t~p~t" ++  ?MODULE_STRING ++ ":"++ ?FUNCTION ++ "~t" ++ Format,[Level,emq_auth_mios_utils:get_time() | Args])).
 -define(LOG_LV(Level,Format),
-  io:format("~p~t~p~t" ++  ?MODULE_STRING ++ ":"++ ?FUNCTION ++ "~t" ++ Format,[Level,emq_auth_mios_utils:get_time())).
+  io:format("~p~t~p~t" ++  ?MODULE_STRING ++ ":"++ ?FUNCTION ++ "~t" ++ Format,[Level,emq_auth_mios_utils:get_time()])).
